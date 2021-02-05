19945af151c387bed7f584a5dc3735a1f4af5297
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2021-02-05 14:48:15 +0100

    First commit with Parcel

---
+++ src/components/App.vue
@@ -0,0 +1,21 @@
+<template> 
+    <div>
+        <button @click="amount++"> Click me </button>
+        The button has been clicked {{amount}} times.
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        amount: 0
+    })
+}
+</script>
+
+<style> 
+    button {
+        border-radius: 10px;
+        padding: 2em;
+    }
+</style>
